a6dbdd351d03b290072544e5c954bc8f32019bb4
Samdish Singh <samdish.singh@codinova.com>
2025-01-24 18:09:16 +0530

    User model

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+namespace Models
+{
+  public enum UserRole {
+    Admin,
+    Customer
+  }
+  public class User
+  {
+    public int Id { set; get; }
+    public required string Name { set; get; }
+    public required string Email { set; get; }
+    public required string PasswordHash { set; get; }
+    public UserRole Role { set; get; }
+    public DateTime? CreatedAt { set; get; }
+    public DateTime? UpdatedAt { set; get; }
+  }
+}
