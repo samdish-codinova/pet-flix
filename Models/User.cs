b2e76482173076d9a2bab0a396bc149ea5afdf5b
Samdish Singh <samdish.singh@codinova.com>
2025-01-30 13:35:56 +0530

    Update user db model which was mistakenly removed from a commit

--- Models/User.cs
+++ Models/User.cs
@@ -9,6 +9,7 @@ namespace Models
     public int Id { set; get; }
     public required string Name { set; get; }
     public required string Email { set; get; }
+    public required string PasswordSalt { set; get; }
     public required string PasswordHash { set; get; }
     public UserRole Role { set; get; }
     public DateTime? CreatedAt { set; get; }
