c7de6b33973af98301902cce0f19311ee4d076f0
Samdish Singh <samdish.singh@codinova.com>
2025-01-31 00:42:43 +0530

    Refactor: BaseUser

--- Models/Dtos/BaseUserDTO.cs
+++ Models/Dtos/BaseUserDTO.cs
@@ -2,9 +2,9 @@ namespace Models
 {
   public class BaseUser
   {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
     public UserRole Role { get; set; }
     public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
