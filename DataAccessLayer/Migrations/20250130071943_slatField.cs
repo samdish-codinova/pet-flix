795f6d520f2214f3e4cfcb1280ce54148ba678d3
Samdish Singh <samdish.singh@codinova.com>
2025-01-30 12:52:58 +0530

    Add migration for salt field

---
+++ DataAccessLayer/Migrations/20250130071943_slatField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class slatField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "User");
+        }
+    }
+}
