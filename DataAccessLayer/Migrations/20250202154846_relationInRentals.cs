51f77d2b94f61a7b990f2eb3f2d94abb29814c4e
Samdish Singh <samdish.singh@codinova.com>
2025-02-02 21:25:18 +0530

    Chore: add relation in rentals model

---
+++ DataAccessLayer/Migrations/20250202154846_relationInRentals.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class relationInRentals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Rental_MovieId",
+                table: "Rental",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rental_UserId",
+                table: "Rental",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rental_Movie_MovieId",
+                table: "Rental",
+                column: "MovieId",
+                principalTable: "Movie",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rental_User_UserId",
+                table: "Rental",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rental_Movie_MovieId",
+                table: "Rental");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rental_User_UserId",
+                table: "Rental");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rental_MovieId",
+                table: "Rental");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rental_UserId",
+                table: "Rental");
+        }
+    }
+}
--- Models/Rental.cs
+++ Models/Rental.cs
@@ -6,7 +6,9 @@ namespace Models
   {
     public required int Id { set; get; }
     public required int MovieId { get; set; }
+    public required Movie Movie { get; set; }
     public required int UserId { get; set; }
+    public required User User { get; set; }
     public required DateTime IssuedDate { get; set; }
     public DateTime ReturnDate { get; set; }
     public required decimal TotalPrice { get; set; }
