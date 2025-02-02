602e044d7d99dadd4992a2fd95ef4332101e12a2
Samdish Singh <samdish.singh@codinova.com>
2025-02-03 00:46:45 +0530

    Chore: update rental model and add migration

--- Models/Rental.cs
+++ Models/Rental.cs
@@ -4,13 +4,13 @@ namespace Models
 {
   public class Rental
   {
-    public required int Id { set; get; }
+    public int Id { set; get; }
     public required int MovieId { get; set; }
-    public required Movie Movie { get; set; }
+    public Movie Movie { get; set; }
     public required int UserId { get; set; }
-    public required User User { get; set; }
+    public User User { get; set; }
     public required DateTime IssuedDate { get; set; }
-    public DateTime ReturnDate { get; set; }
+    public DateTime? ReturnDate { get; set; } = null;
     public required decimal TotalPrice { get; set; }
   }
 }
